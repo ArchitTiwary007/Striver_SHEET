1abdb6f791b2b411df93871afdee6f1074dab86b
Archit Tiwary <tiwaryart007@gmail.com>
2026-01-15 21:37:42 +0530

    Add 53_maximum_subarray.cpp - LeetCode Problem 53

---
+++ 53_maximum_subarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n=nums.size();
+        long long maxsum=LONG_MIN;
+        long long sum=0;
+        for(int i=0;i<n;i++)
+        {            
+              sum+=nums[i];
+              
+              if(sum>maxsum)
+              maxsum=sum;
+
+              if(sum<0)
+              sum=0;
+        }
+         return maxsum;
+    }
+};
