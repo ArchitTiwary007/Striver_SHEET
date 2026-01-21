489dfb3edea429265071c302e13cc44b78cd0acb
Archit Tiwary <tiwaryart007@gmail.com>
2026-01-22 00:39:45 +0530

    Update 48_rotate_image.cpp - LeetCode Problem 48

--- 48_rotate_image.cpp
+++ 48_rotate_image.cpp
@@ -41,9 +41,19 @@ public:
             swap(matrix[i][j],matrix[j][i]);//transpose!!
         }
     }
+    // for(int i=0;i<n;i++)
+    // {
+    //     reverse(matrix[i].begin(),matrix[i].end());
+    // }
     for(int i=0;i<n;i++)
     {
-        reverse(matrix[i].begin(),matrix[i].end());
+        int start=0,end=n-1;
+        while(start<end)
+        {
+            swap(matrix[i][start],matrix[i][end]);
+            start++,end--;
+        }
+
     }
     }
 };
