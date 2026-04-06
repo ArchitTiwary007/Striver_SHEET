96693b2e95e96c8cdcd9702f06bbc01caab0ed53
Archit Tiwary <tiwaryart007@gmail.com>
2026-04-06 08:14:34 +0530

    Add reverseList function to reverse a linked list
    Implement reverse linked list using a vector to store values.

---
+++ 206_Reverse Linked List.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+    
+      //METHOD-2
+    //   ListNode* curr=head,*prev=NULL,*fut=NULL;
+    //   while(curr)
+    //   {
+    //     fut=curr->next;
+    //     curr->next=prev;
+    //     prev=curr;
+    //     curr=fut;
+    //   };
+    //   head=prev;
+    // return head;
+
+    //METHOD-1
+    vector<int>ans;
+    ListNode*temp=head;
+    while(temp)
+    {
+        ans.push_back(temp->val);
+        temp=temp->next;
+    }
+    int i=ans.size()-1;
+    temp=head;//reset
+    while(temp)
+    {
+        temp->val=ans[i];
+        i--;
+        temp=temp->next;
+    }
+    return head;
+
+    }
+};
